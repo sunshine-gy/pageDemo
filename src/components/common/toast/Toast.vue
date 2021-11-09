38e579c2aa3410e4956c45301f551f8e8efb3d94
gongyun <858702953@qq.com>
2021-11-09 14:19:34 +0800

    添加toast功能

---
+++ src/components/common/toast/Toast.vue
@@ -0,0 +1,38 @@
+<template>
+  <transition name="fade">
+    <div class="toast" v-show="isShow">{{message}}</div>
+  </transition>
+</template>
+
+<script>
+	export default {
+		name: "Toast",
+    data() {
+			return {
+				isShow: false,
+                message: '我是弹窗'
+      }
+    }
+	}
+</script>
+
+<style scoped>
+  .toast {
+    position: fixed;
+    left: 50%;
+    top: 36%;
+    background-color: pink;
+    color: rgb(245, 101, 18);
+    padding: 20px;
+    border-radius: 10px;
+    transform: translate(-50%,-50%);
+    z-index: 99;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Toast from './components/common/toast'
 
 Vue.config.productionTip = false
+
+Vue.use(Toast)
 
 new Vue({
   router,
